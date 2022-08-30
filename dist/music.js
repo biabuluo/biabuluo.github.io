5005a9b4211f47aee375cc3ed11f6e2e33e49da2
biabuluo <1802362935@qq.com>
2022-08-30 14:37:48 +0800

    Site updated: 2022-08-30 14:37:47

---
+++ dist/music.js
@@ -0,0 +1,25 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: 'Hanezeve Caradhina',
+        artist: 'Kevin Penkin',
+        url: 'https://m701.music.126.net/20220830145054/9cfc07acffebdeb841c4c8b3931d3f98/jdyyaac/5358/065c/0e58/d23c3811b21634ea27f5920d64d5e004.m4a',
+        cover: 'http://p1.music.126.net/yZndmoC6UEsRZeyonfjahg==/109951163031981246.jpg?param=130y130',
+      },
+      {
+        name: 'Endless Embrace',
+        artist: 'MYTH & ROID',
+        url: 'https://m701.music.126.net/20220830145337/b261a10ac3de1e46a597955b3ba386e7/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/17807014832/c2a9/d6bc/2eb6/c001b5ba5b4406901126cade2b01706d.m4a',
+        cover: 'http://p1.music.126.net/-H0kjb6trH38D-If8NbRxA==/109951167811038018.jpg?param=130y130',
+      },
+      {
+        name: "形状",
+        artist: '安月名莉子',
+        url: 'https://m701.music.126.net/20220830144726/24eac4052020afc202e0a6a2e69a6bf1/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/17806528270/a917/3425/e6d3/5f87507db17e7dca57426cfb6890b565.m4a',
+        cover: 'http://p1.music.126.net/H0xkmdZ2KhAvApLJMw9Dag==/109951167811003416.jpg?param=130y130',
+      }
+    ]
+});
